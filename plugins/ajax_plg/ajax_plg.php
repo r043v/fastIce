3f8189e2a74472ba9590a8fa47efa04dc4a0cf2d
r043v <noferov@gmail.com>
2013-04-29 18:15:49 +0200

    example plugin update

--- plugins/ajax_plg/ajax_plg.php
+++ plugins/ajax_plg/ajax_plg.php
@@ -1,3 +1,3 @@
-<?php	addToJs('function ajaxPluginCall(plg,args,callback){$.post("'.site_url.module_path.'/ajaxplg/ajax.php?plg="+encodeURIComponent(plg), args, callback);}');
+<?php	addToJs('function ajaxPluginCall(plg,args,callback){$.post("'.site_url.module_path.'/ajax_plg/ajax.php?plg="+encodeURIComponent(plg), args, callback);}');
 	function fn_ajax_plg($args){}
 ?>
